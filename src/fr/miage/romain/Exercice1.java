9a2e4c58513134e475d6d7589ba980b12582ef41
Romain JAFFE <romain.jaffe@gmail.com>
2019-02-11 13:27:10 +0100

    Création première méthode recursive pour afficher contenu dossier

---
+++ src/fr/miage/romain/Exercice1.java
@@ -0,0 +1,30 @@
+package fr.miage.romain;
+
+import java.io.File;
+
+public class Exercice1 {
+
+	public static void main(String[] args) {
+	
+		String pathFile = ".";
+		
+		toStringFile(pathFile);
+
+	}
+	
+	public static void toStringFile(String pathFile){
+		
+		for(File file : new File(pathFile).listFiles()){
+			
+			if(file.isDirectory()){
+				System.out.println(file.toString());
+				toStringFile(file.getPath());
+			} else {
+				System.out.println(file.toString());
+			}
+			
+		}
+		
+	}
+
+}
