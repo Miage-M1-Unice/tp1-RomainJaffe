b2a861a434590beac157cc71a3dbe07b84fea64f
Romain JAFFE <romain.jaffe@gmail.com>
2019-02-19 11:06:38 +0100

    Début TP2

--- src/fr/miage/romain/Tp2.java
+++ src/fr/miage/romain/Tp2.java
@@ -1,10 +1,111 @@
 package fr.miage.romain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Tp2 {
 
-	public static void main(String[] args) {
-		System.out.println("coucou");
+	public static void analyseClasse(String nomClasse) throws ClassNotFoundException {
+	    // Récupération d'un objet de type Class correspondant au nom passé en paramètres
+	    Class cl = getClasse(nomClasse);
 
-	}
+	    afficheEnTeteClasse(cl);
+
+	    System.out.println();
+	    afficheAttributs(cl);
+
+	    System.out.println();
+	    afficheConstructeurs(cl);
+
+	    System.out.println();
+	    afficheMethodes(cl);
+
+	    // L'accolade fermante de fin de classe !
+	    System.out.println("}");
+	  }
+
+
+	  /** Retourne la classe dont le nom est passé en paramètre */
+	  public static Class getClasse(String nomClasse) throws ClassNotFoundException {
+	    return Class.forName(nomClasse);
+	  }
+
+	  /** Cette méthode affiche par ex "public class Toto extends Tata implements Titi, Tutu {" */
+	  public static void afficheEnTeteClasse(Class cl) {
+	    //  Affichage du modifier et du nom de la classe
+	    // CODE A ECRIRE
+
+	   // Récupération de la superclasse si elle existe (null si cl est le type Object)
+	    //Class supercl = // CODE A ECRIRE
+
+	    // On ecrit le "extends " que si la superclasse est non nulle et
+	    // différente de Object
+	    // CODE A ECRIRE
+
+	    // Affichage des interfaces que la classe implemente
+	    // CODE A ECRIRE
+
+	    // Enfin, l'accolade ouvrante !
+	    System.out.print(" {\n");
+	    
+	  }
+
+	  public static void afficheAttributs(Class cl) {
+		  
+		System.out.println("Affichage des attributs");
+		  
+	    for(Field f : cl.getFields()){
+	    	System.out.println(f.toString());
+	    }
+	  }
+
+	  public static void afficheConstructeurs(Class cl) {
+	    
+		  System.out.println("Affichage des constructeurs");
+
+		  
+		  for(Constructor c : cl.getConstructors()){
+			  System.out.println(c.toString());
+		  }
+	    
+	  }
+
+	  public static void afficheMethodes(Class cl) {
+	    
+		  System.out.println("Affichage des méthodes");
+		  
+		  for(Method m : cl.getMethods()){
+			  System.out.println(m.toString());
+		  }
+		  
+	  }
+
+	  public static String litChaineAuClavier() throws IOException {
+	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	      return br.readLine();
+	  }
+
+	  public static void main(String[] args) {
+	    boolean ok = false;
+
+	    while(!ok) {
+	      try {
+	        System.out.print("Entrez le nom d'une classe (ex : java.util.Date): ");
+	        String nomClasse = litChaineAuClavier();
+
+	        analyseClasse(nomClasse);
+
+	        ok = true;
+	      } catch(ClassNotFoundException e) {
+	        System.out.println("Classe non trouvée.");
+	      }catch(IOException e) {
+	        System.out.println("Erreur d'E/S!");
+	      }
+	    }
+	  }
 
 }
